3a00e447d864e9bf26e145aae82ddaba431580e7
Tanuj <tanujchoudhary001@gmail.com>
2020-09-25 12:37:49 +0530

    Created express server

---
+++ server.js
@@ -0,0 +1,32 @@
+// Third Party Imports
+require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
+
+// Project Imports
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+
+// LOCAL DATABASE
+const DB = process.env.DATABASE_LOCAL;
+
+// Connect to database
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+      console.log('database connected');
+    }
+  })
+  .catch((err) => {
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+      console.log(err);
+    }
+  });
+
+// Start server
+app.listen(PORT, () => {
+  if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    console.log('Listening at port 3000');
+  }
+});
