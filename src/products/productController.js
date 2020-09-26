ce09f4c9a55a0ac023eac393eb5a9d99183a5670
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 16:27:14 +0530

    Added get product function

--- src/products/productController.js
+++ src/products/productController.js
@@ -1,4 +1,30 @@
 const Products = require('./productModel');
+const AppError = require('../error/appError');
+
+const getProduct = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return next(new AppError('Please provide the ID', 401));
+  }
+
+  try {
+    const product = await Products.findById(id);
+
+    if (!product) {
+      return next(new AppError('No product found with that id', 401));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 const getAllProducts = async (req, res, next) => {
   try {
@@ -17,4 +43,5 @@ const getAllProducts = async (req, res, next) => {
 
 module.exports = {
   getAllProducts,
+  getProduct,
 };
