478525cebfcfd7391383b9864cf2ce7a6e397e84
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 17:08:34 +0530

    Fixed a typo

--- src/products/productModel.js
+++ src/products/productModel.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
   name: {
     type: String,
-    reqiured: [true, 'Product name is required'],
+    required: [true, 'Product name is required'],
     unique: true,
   },
   brand: {
     type: String,
-    reqiured: [true, 'Brand name is required'],
+    required: [true, 'Brand name is required'],
   },
   price: {
     type: String,
-    reqiured: [true, 'Product price is required'],
+    required: [true, 'Product price is required'],
   },
   dialShape: {
     type: String,
