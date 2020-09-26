a7bc5a2054539e23705d871dfa816163e6e0ad31
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 15:27:21 +0530

    Added error handling in get all products

--- src/products/productController.js
+++ src/products/productController.js
@@ -1,12 +1,18 @@
 const Products = require('./productModel');
 
 const getAllProducts = async (req, res, next) => {
-  const products = await Products.find({});
+  try {
+    const products = await Products.find({});
 
-  res.status(200).json({
-    status: 'success',
-    products,
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        products,
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 module.exports = {
