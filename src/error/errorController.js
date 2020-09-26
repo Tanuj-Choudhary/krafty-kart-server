f50b2e7b7a4b01cebbaa75871aa92dbf3201498b
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 16:28:15 +0530

    Added cast error to prod errors

--- src/error/errorController.js
+++ src/error/errorController.js
@@ -1,10 +1,18 @@
-const { sendErrorDev, sendErrorProd } = require('./utils');
+const { sendErrorDev, sendErrorProd, handleDBCastError } = require('./utils');
 
 const errorController = (err, req, res, next) => {
-  if (process.NODE_ENV === 'DEVELOPMENT') {
+  if (process.env.NODE_ENV === 'DEVELOPMENT') {
     sendErrorDev(err, res);
-  } else if (process.NODE_ENV === 'PRODUCTION') {
-    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === 'PRODUCTION') {
+    // Extract error to new object
+    let error = { ...err };
+    error.name = err.name;
+    error.message = err.message;
+
+    // Handle specific production errors
+    if (error.name === 'CastError') error = handleDBCastError(error);
+
+    sendErrorProd(error, res);
   }
 };
 
