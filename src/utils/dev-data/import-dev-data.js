e5e3ed12748e35a9c2891f86629c2437bfa04c83
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 14:52:55 +0530

    Added script to import and delete data

---
+++ src/utils/dev-data/import-dev-data.js
@@ -0,0 +1,59 @@
+require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Products = require('../../products/productModel');
+
+// LOCAL DATABASE
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+      console.log('database connected');
+    }
+  })
+  .catch((err) => {
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+      console.log(err);
+    }
+  });
+
+// Read JSON file
+const products = JSON.parse(
+  fs.readFileSync(`${__dirname}/devData.json`, 'utf-8')
+);
+
+// Import Data into DB
+const importData = async () => {
+  try {
+    await Products.create(products);
+    console.log('Data succesfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+// Delete All Data from DB
+const deleteData = async () => {
+  try {
+    await Products.deleteMany({});
+    console.log('Data succesfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
