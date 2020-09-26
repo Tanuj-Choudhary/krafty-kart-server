e8f060a5e809d56e2293e1436c97a5d2bf3f0a36
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 17:09:41 +0530

    Added validation and mongo error handler

--- src/error/errorController.js
+++ src/error/errorController.js
@@ -1,4 +1,10 @@
-const { sendErrorDev, sendErrorProd, handleDBCastError } = require('./utils');
+const {
+  sendErrorDev,
+  sendErrorProd,
+  handleDBCastError,
+  handleDBValidationError,
+  handleDBMongoError,
+} = require('./utils');
 
 const errorController = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'DEVELOPMENT') {
@@ -11,6 +17,9 @@ const errorController = (err, req, res, next) => {
 
     // Handle specific production errors
     if (error.name === 'CastError') error = handleDBCastError(error);
+    if (error.name === 'MongoError') error = handleDBMongoError(error);
+    if (error.name === 'ValidationError')
+      error = handleDBValidationError(error);
 
     sendErrorProd(error, res);
   }
