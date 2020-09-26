1df19489dc274580835a86a01406ba8179b23c15
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 18:45:28 +0530

    Using handler factory on products

--- src/products/productController.js
+++ src/products/productController.js
@@ -1,96 +1,18 @@
 const Products = require('./productModel');
-const AppError = require('../error/appError');
 
-const updateProduct = async (req, res, next) => {
-  try {
-    const product = await Products.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-      useFindAndModify: false,
-    });
+const {
+  createOne,
+  updateOne,
+  deleteOne,
+  getAll,
+  getOne,
+} = require('../utils/handlerFactory');
 
-    if (!product) {
-      return next(new AppError('No product found with that ID', 401));
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        product,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const deleteProduct = async (req, res, next) => {
-  try {
-    const product = await Products.findByIdAndDelete(req.params.id, {
-      useFindAndModify: false,
-    });
-
-    if (!product) {
-      return next(new AppError('No product found with that ID', 401));
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const createProduct = async (req, res, next) => {
-  try {
-    const product = await Products.create(req.body);
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        product,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const getProduct = async (req, res, next) => {
-  try {
-    const product = await Products.findById(req.params.id);
-
-    if (!product) {
-      return next(new AppError('No product found with that id', 401));
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        product,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const getAllProducts = async (req, res, next) => {
-  try {
-    const products = await Products.find({});
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        products,
-      },
-    });
-  } catch (err) {
-    return next(err);
-  }
-};
+const updateProduct = updateOne(Products);
+const deleteProduct = deleteOne(Products);
+const createProduct = createOne(Products);
+const getProduct = getOne(Products);
+const getAllProducts = getAll(Products);
 
 module.exports = {
   getAllProducts,
