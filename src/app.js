95a12bbc60e5d28845ec64cb4104b8042d2a58c2
Tanuj <tanujchoudhary001@gmail.com>
2020-09-26 19:11:43 +0530

    Added body parser, product route and global error handler middleware to app

--- src/app.js
+++ src/app.js
@@ -2,11 +2,26 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// Project Imports
+const globalErrorHandler = require('./error/errorController');
+const productRouter = require('./products/productRouter');
+const AppError = require('./error/appError');
+
 const app = express();
 
 // MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
+
+// SEE API REQUEST
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
   app.use(morgan('dev'));
 }
+
+//  Body parser
+app.use(express.json());
+
+// Routes
+app.use('/api/v1/products', productRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
