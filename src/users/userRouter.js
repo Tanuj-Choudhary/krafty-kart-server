11d342464847a05f5e9d5f44e240c9d331ae0495
Tanuj <tanujchoudhary001@gmail.com>
2020-09-28 17:07:55 +0530

    Changed user routes

--- src/users/userRouter.js
+++ src/users/userRouter.js
@@ -10,10 +10,26 @@ const {
   createUser,
 } = require('./userController');
 
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  protect,
+  restrictTo,
+} = require('../auth/authController');
+
 const router = express.Router();
 
-router.route('/').get(getAllUser).post(createUser);
+router.route('/signup').post(signup);
+router.route('/login').post(login);
+router.route('/forgotPassword').post(forgotPassword);
+router.route('/resetPassword').post(resetPassword);
 
+// Restrict below routes to admin
+router.use(protect, restrictTo('admin'));
+
+router.route('/').get(getAllUser).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
