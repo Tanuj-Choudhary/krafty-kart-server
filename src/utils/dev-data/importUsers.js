873c644cad441d3b4d6d79c6accb695fa0e89bf7
Tanuj <tanujchoudhary001@gmail.com>
2020-10-07 20:50:23 +0530

    Added import users to utils

---
+++ src/utils/dev-data/importUsers.js
@@ -0,0 +1,57 @@
+require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Users = require('../../users/usersModel');
+
+// LOCAL DATABASE
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+      console.log('database connected');
+    }
+  })
+  .catch((err) => {
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+      console.log(err);
+    }
+  });
+
+// Read JSON file
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+
+// Import Data into DB
+const importData = async () => {
+  try {
+    await Users.create(users);
+    console.log('Data succesfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+// Delete All Data from DB
+const deleteData = async () => {
+  try {
+    await Users.deleteMany({});
+    console.log('Data succesfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
