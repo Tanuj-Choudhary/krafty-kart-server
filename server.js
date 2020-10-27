75a46e2120e66afc9cbf5d0e914922205dc3299b
Tanuj <tanujchoudhary001@gmail.com>
2020-10-27 22:17:01 +0530

    Handling uncaught exception

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 require('dotenv').config({ path: './config.env' });
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // Project Imports
 const app = require('./src/app');
 
@@ -29,8 +35,23 @@ mongoose
   });
 
 // Start server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   if (process.env.NODE_ENV === 'DEVELOPMENT') {
     console.log('Listening at port 8000');
   }
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
 });
